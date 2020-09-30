993fe4da42b5773bed2aba1d3f2d5e65a0081582
Jason Gwon <animagig@gmail.com>
2020-09-30 17:14:30 +0900

    Updates

---
+++ precache-manifest.1b8b46ef4f061422d22b00aaafebfedd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adea8bea125b01d97029b91d12ddf4e4",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "10a8a0a4b599cc66b911",
+    "url": "/movie_app_2020/static/css/main.c9eed117.chunk.css"
+  },
+  {
+    "revision": "8c9e848948e16e7bd51c",
+    "url": "/movie_app_2020/static/js/2.a0822667.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.a0822667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a8a0a4b599cc66b911",
+    "url": "/movie_app_2020/static/js/main.b42bd66f.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
